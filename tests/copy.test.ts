80605f44c41b63a79f1617d4a3db73f84c5994f6
joel-statsig <joel@statsig.com>
2026-05-02 13:43:58 -0700

    [jj-spr] initial version
    Created using jj-spr 0.1.0

--- tests/copy.test.ts
+++ tests/copy.test.ts
@@ -1,11 +1,12 @@
 import * as io from '@actions/io'
-import { copyExtraFiles } from '../src/copy'
+import { expect, it, vi } from 'vitest'
+import { copyExtraFiles } from '../src/copy.js'
 
-jest.mock('@actions/io')
-const mkdirPMock = io.mkdirP as jest.Mock<Promise<void>>
-const cpMock = io.cp as jest.Mock<Promise<void>>
+vi.mock('@actions/io')
+const mkdirPMock = vi.mocked(io).mkdirP
+const cpMock = vi.mocked(io).cp
 
-test('run successfully', async () => {
+it('run successfully', async () => {
   mkdirPMock.mockResolvedValue()
   cpMock.mockResolvedValue()
 
@@ -15,6 +16,6 @@ test('run successfully', async () => {
   expect(mkdirPMock).toHaveBeenCalledWith(`/tmp/foo/fixtures/overlays/development`)
   expect(cpMock).toHaveBeenCalledWith(
     `${__dirname}/fixtures/overlays/development/metadata.yaml`,
-    `/tmp/foo/fixtures/overlays/development/metadata.yaml`
+    `/tmp/foo/fixtures/overlays/development/metadata.yaml`,
   )
 })
